45bb919472b480ad9694d2c66e8d87b371552b4c
Vanshika Raj Sharma <64151910+vanshika2901@users.noreply.github.com>
2021-02-07 13:12:42 +0530

    Add files via upload

---
+++ photo gallery/photo gallery.js
@@ -0,0 +1,8 @@
+function upDate(previewPic){
+	document.getElementById("image").style.backgroundImage= "url('"+ previewPic.src +"')";
+	document.getElementById("image").innerHTML = previewPic.alt;
+}
+function unDo(){
+document.getElementById("image").innerHTML = "Hover over an image below to diplay here";
+	document.getElementById("image").style.backgroundImage= "url('')";
+}
